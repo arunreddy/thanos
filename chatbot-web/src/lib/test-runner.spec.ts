84d78ea240289cedf82b3d4d2d2d302af34162dc
Arun Reddy <arunreddy.nelakurthi@gmail.com>
2025-04-15 12:13:04 -0700

    fix issues

--- chatbot-web/src/lib/test-runner.spec.ts
+++
@@ -1,9 +0,0 @@
-import './test-runner';
-
-describe('Test Runner', () => {
-  it('should execute all utils functions', () => {
-    // This just imports the test-runner module
-    // The actual tests are in utils.spec.ts
-    expect(true).toBe(true);
-  });
-});
