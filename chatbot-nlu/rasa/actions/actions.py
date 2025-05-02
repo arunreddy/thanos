1aeddfc6b2b2bff62dbc4aad6dc7ca17b26596c1
Manoj Naidu Yandrapu <manojnaiduyandrapu@gmail.com>
2025-05-02 15:24:57 -0700

    added delete database, removed the database types to user text input for both create and delete database

--- chatbot-nlu/rasa/actions/actions.py
+++ chatbot-nlu/rasa/actions/actions.py
@@ -140,3 +140,26 @@ class ActionSubmitDatabase(Action):
         )
 
         return []
+    
+class ActionSubmitDeleteDatabase(Action):
+    def name(self) -> Text:
+        return "action_submit_delete_database"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: dict
+    ) -> List[dict]:
+        db_name    = tracker.get_slot("database_name")
+        db_version = tracker.get_slot("database_version")
+        sysid      = tracker.get_slot("sysid")
+
+        # TODO: call your delete API / issue ticket, etc.
+        dispatcher.utter_message(
+            text=(
+                f"Your request to *delete* {db_name} "
+                f"version {db_version} with SysID {sysid} has been submitted!"
+            )
+        )
+        return []
